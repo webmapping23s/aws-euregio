b731be50e962786379cf2e26d2da11d62b279918
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-05-11 16:48:07 +0200

    add filter for invalid temperatures

--- main.js
+++ main.js
@@ -69,9 +69,15 @@ function writeStationLayer(jsondata) {
 
 function writeTemperatureLayer(jsondata) {
     L.geoJSON(jsondata, {
+        filter: function(feature) {
+            if (feature.properties.LT > -50 && feature.properties.LT < 50) {
+                return true;
+            }
+        },
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {
                 icon: L.divIcon({
+                    className: "aws-div-icon",
                     html: `<span>${feature.properties.LT}</span>`
                 })
             });
