8e72a6a4434a9427b5887489d8fd165b803ee009
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-05-11 09:11:24 +0200

    WL7: template auspacken

---
+++ main.js
@@ -0,0 +1,45 @@
+/* Wetterstationen Euregio Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map", {
+    fullscreenControl: true
+}).setView([ibk.lat, ibk.lng], 11);
+
+// thematische Layer
+let themaLayer = {
+    stations: L.featureGroup()
+}
+
+// Hintergrundlayer
+let layerControl = L.control.layers({
+    "Relief avalanche.report": L.tileLayer(
+        "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+        attribution: `© <a href="https://lawinen.report">CC BY avalanche.report</a>`
+    }).addTo(map),
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
+}, {
+    "Wetterstationen": themaLayer.stations.addTo(map)
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Vienna Sightseeing Haltestellen
+async function showStations(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+
+    // Wetterstationen mit Icons und Popups implementieren
+
+}
+showStations("https://static.avalanche.report/weather_stations/stations.geojson");
