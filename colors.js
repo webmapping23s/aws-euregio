f943bbc885e173e40c4b65a51e3f7e9de55736ad
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-05-25 09:03:15 +0200

    WL8: Farben für den Schneehöhenlayer hinzufügen

--- colors.js
+++ colors.js
@@ -96,5 +96,52 @@ const COLORS = {
             max: 999,
             color: "#ff3232",
         },
+    ],
+    snow: [
+        {
+            min: 0,
+            max: 1,
+            color: "#fff",
+        },
+        {
+            min: 1,
+            max: 10,
+            color: "#ffffb2",
+        },
+        {
+            min: 10,
+            max: 25,
+            color: "#b0ffbc",
+        },
+        {
+            min: 25,
+            max: 50,
+            color: "#8cffff",
+        },
+        {
+            min: 50,
+            max: 100,
+            color: "#19cdff",
+        },
+        {
+            min: 100,
+            max: 200,
+            color: "#1982ff",
+        },
+        {
+            min: 200,
+            max: 300,
+            color: "#0f5abe",
+        },
+        {
+            min: 300,
+            max: 400,
+            color: "#784bff",
+        },
+        {
+            min: 400,
+            max: 9999,
+            color: "#cd0feb",
+        }
     ]
 }
